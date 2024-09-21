423daa198bcc10a8f37b1e2f7d5eb343ab1e7977
guiurm <guiurm@gmail.com>
2024-09-21 16:22:36 +0200

    feat(tabs): add tab components

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,25 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
+import NavbarComponent from './components/NavbarComponent.vue';
 </script>
 
 <template>
-    <RouterView />
+    <navbar-component />
+    <router-view #default="{ Component }">
+        <transition name="fade">
+            <component :is="Component" />
+        </transition>
+    </router-view>
 </template>
+
+<style>
+.fade-enter-active,
+.fade-leave-active {
+    transition: opacity 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+    opacity: 0;
+}
+</style>
---
+++ src/components/tabs/TabSlide.vue
@@ -0,0 +1,67 @@
+<script setup lang="ts">
+import {
+    computed,
+    inject,
+    onBeforeMount,
+    onBeforeUnmount,
+    ref,
+    watch,
+    type Component,
+    type Ref,
+    type VNode
+} from 'vue';
+import useCssClassTranslator, { type TCss } from '../cssClassTranslator';
+import { TAB_ADD, TAB_REMOVE, TAB_SET_ACTIVE, type TTabInjections, type TTabSlideData } from './TabGlobals';
+
+const props = withDefaults(
+    defineProps<{
+        header: string | { component: Component | (() => VNode); binds: Record<string, any> };
+        id?: string;
+        active?: boolean;
+        transitionName?: string;
+        class?: TCss;
+    }>(),
+    { active: false, transitionName: 'fade', class: '' }
+);
+const emits = defineEmits<{ clickTab: []; show: []; hide: [] }>();
+
+const id = props.id ?? parseInt(`${Date.now() * Math.random()}`).toString(16);
+const tabR = ref({}) as Ref<TTabSlideData>;
+
+const { css, ...modifyCss } = useCssClassTranslator(props.class);
+modifyCss.addCss('overflow-y-auto flex flex-col grow');
+watch(
+    () => props.class,
+    n => modifyCss.addCss(n)
+);
+
+const setTab = inject(TAB_SET_ACTIVE) as TTabInjections[typeof TAB_SET_ACTIVE];
+
+const addTab = inject(TAB_ADD) as TTabInjections[typeof TAB_ADD];
+const removeTab = inject(TAB_REMOVE) as TTabInjections[typeof TAB_REMOVE];
+
+onBeforeMount(() => {
+    tabR.value = addTab({
+        header: props.header,
+        id,
+        active: props.active,
+        emit: { clickTab: () => emits('clickTab'), show: () => emits('show'), hide: () => emits('hide') }
+    }).value;
+});
+onBeforeUnmount(() => removeTab(id));
+
+watch(
+    () => props.active,
+    n => {
+        if (n) setTab(id);
+    }
+);
+const transition = computed(() => props.transitionName);
+</script>
+<template>
+    <transition :name="transition">
+        <div v-if="tabR.active" :class="css">
+            <slot />
+        </div>
+    </transition>
+</template>
