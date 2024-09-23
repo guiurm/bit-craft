dd2d60281a695e79a6580649c924adb7a8e54ee2
guiurm <guiurm@gmail.com>
2024-09-23 11:30:41 +0200

    wip(template): add btn component

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import plugin from "tailwindcss/plugin";
+
 /** @type {import('tailwindcss').Config} */
 export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,vue}"],
@@ -72,5 +74,67 @@ export default {
             },
         },
     },
-    plugins: [],
+    plugins: [
+        plugin(function ({ addComponents }) {
+            addComponents({
+                ".btn": genBtn(),
+                /*".btn-sm": {
+                    //"@apply btn": {},
+                    fontSize: theme("fontSize.sm"),
+                },
+                ".tab-btn": {
+                    "@apply  py-1 px-2 cursor-pointer text-white bg-grey-400 border-l-0 border-0 border-r border-r-white":
+                        {},
+                    "&:last-child": {
+                        //   border: theme("colors.primary.900"),
+                        borderRight: "0px",
+                    },
+                    "&:hover": {
+                        backgroundColor: theme("colors.grey.500"),
+                    },
+                },
+                ".tab-btn--active": {
+                    "@apply tab-btn bg-primary-500": {},
+                    "&:hover": {
+                        backgroundColor: theme("colors.primary.600"),
+                    },
+                },
+                ".glass": {
+                    "@apply backdrop-blur backdrop-saturate-[182%] bg-white/50 px-4 py-2 shadow-2xl rounded-md": {},
+                    border: "1px solid rgba(255, 255, 255, 0.125)",
+                },*/
+            });
+        }),
+    ],
 };
+
+//  focus:ring-4 focus:ring-blue-300  dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800
+
+/* eslint-disable no-unused-vars */
+const btn = "font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 focus:outline-none";
+const btnRouded = "rounded-full";
+
+const btnOutline = "focus:ring-4 font-medium text-sm px-5 py-2.5 text-center me-2 mb-2";
+
+const btnxs = "px-3 py-2 text-xs font-medium";
+const btnsm = "px-3 py-2 text-sm font-medium";
+const btnlg = "px-5 py-3 text-base font-medium";
+const btnxl = "px-6 py-3.5 text-base font-medium";
+
+function genBtn() {
+    return {
+        [`@apply ${btn}`]: {},
+        "&-xs": {
+            [`@apply ${btnxs}`]: {},
+        },
+        "&-sm": {
+            [`@apply ${btnsm}`]: {},
+        },
+        "&-lg": {
+            [`@apply ${btnlg}`]: {},
+        },
+        "&-xl": {
+            [`@apply ${btnxl}`]: {},
+        },
+    };
+}
