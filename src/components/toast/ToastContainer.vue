2a6efbf16292cda50076825f50d0bd131ae65e19
guiurm <guiurm@gmail.com>
2024-10-15 12:55:07 +0200

    wip(toast): add sass and position management for items

---
+++ src/components/toast/ToastContainer.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { computed, ref } from 'vue';
+import ToastItem from './ToastItem.vue';
+
+const positions = ['top-right', 'top-center', 'top-left', 'bottom-right', 'bottom-center', 'bottom-left'] as const;
+const positionIndex = ref(0);
+const position = computed(() => positions[positionIndex.value]);
+
+let a = setInterval(() => {
+    if (positionIndex.value === positions.length - 1) clearInterval(a);
+    positionIndex.value = positionIndex.value === positions.length - 1 ? 0 : positionIndex.value + 1;
+}, 2000);
+</script>
+<template>
+    <div class="relative">
+        {{ position }}
+        <toast-item target="document" :position="position" />
+        <toast-item target="document" :position="position" />
+    </div>
+</template>
--- src/views/ToastsView.vue
+++ src/views/ToastsView.vue
@@ -1,4 +1,5 @@
 <script lang="ts" setup>
+import ToastContainer from '@/components/toast/ToastContainer.vue';
 import ToastItem from '@/components/toast/ToastItem.vue';
 import MainLayout from '@/layouts/MainLayout.vue';
 import { ref } from 'vue';
@@ -13,5 +14,6 @@ const toggle = () => {
         {{ show }}
         <div class="btn" @click="toggle">toggle</div>
         <toast-item :live-time="3000" :show-life-time="true" v-model="show"> Some message in toast</toast-item>
+        <toast-container />
     </main-layout>
 </template>
