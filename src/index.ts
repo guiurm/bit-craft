6a5ab66d0e6b414596219f64d580afb56d07a2be
guiurm <guiurm@gmail.com>
2024-10-18 19:09:11 +0200

    build: add config for lib build

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,4 @@
 import type { App, ObjectPlugin } from 'vue';
-import CarouselCard from './components/carousel/CarouselCard.vue';
-import CarouselContainer from './components/carousel/CarouselContainer.vue';
-import CDatatable from './components/datatable/CDatatable.vue';
-import CDatatableCell from './components/datatable/CDatatableCell.vue';
-import CDatatableCellHeader from './components/datatable/CDatatableCellHeader.vue';
-import CDatatableHeader from './components/datatable/CDatatableHeader.vue';
-import CDatatableRow from './components/datatable/CDatatableRow.vue';
-import DragableSortable from './components/dragable/DragableSortable.vue';
-import DropdownComponent from './components/dropdown/DropdownComponent.vue';
-import BaseModal from './components/modal/BaseModal.vue';
-import StyledModal from './components/modal/StyledModal.vue';
-import TabMenu from './components/tabs/TabMenu.vue';
-import TabSlide from './components/tabs/TabSlide.vue';
-import ToastContainer from './components/toast/ToastContainer.vue';
-import ToastItem from './components/toast/ToastItem.vue';
 import { onClickOutside } from './composables/onClickOutside';
 import { noop, type TNopp } from './globals';
 
@@ -21,33 +6,33 @@ class BitCraft implements ObjectPlugin {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     install(app: App, ..._options: any[]) {
         // toast
-        app.component('toast-container', ToastContainer);
-        app.component('toast-item', ToastItem);
+        //app.component('toast-container', ToastContainer);
+        //app.component('toast-item', ToastItem);
 
         // carousel
-        app.component('carousel-container', CarouselContainer);
-        app.component('carousel-card', CarouselCard);
+        //app.component('carousel-container', CarouselContainer);
+        //app.component('carousel-card', CarouselCard);
 
         // datatable
-        app.component('c-datatable-', CDatatable);
-        app.component('c-datatable-header', CDatatableHeader);
-        app.component('c-datatable-row', CDatatableRow);
-        app.component('c-datatable-cell-header', CDatatableCellHeader);
-        app.component('c-datatable-cell', CDatatableCell);
+        //app.component('c-datatable-', CDatatable);
+        //app.component('c-datatable-header', CDatatableHeader);
+        //app.component('c-datatable-row', CDatatableRow);
+        //app.component('c-datatable-cell-header', CDatatableCellHeader);
+        //app.component('c-datatable-cell', CDatatableCell);
 
         // dragable
-        app.component('dragable-sortable', DragableSortable);
+        //app.component('dragable-sortable', DragableSortable);
 
         // dropdown
-        app.component('dropdown-component', DropdownComponent);
+        //app.component('dropdown-component', DropdownComponent);
 
         // modal
-        app.component('base-modal', BaseModal);
-        app.component('styled-modal', StyledModal);
+        //app.component('base-modal', BaseModal);
+        //app.component('styled-modal', StyledModal);
 
         // tabs
-        app.component('tab-menu', TabMenu);
-        app.component('tab-slide', TabSlide);
+        //app.component('tab-menu', TabMenu);
+        //app.component('tab-slide', TabSlide);
 
         const outClickList = new Map<HTMLElement, TNopp>();
         app.directive('outside-click', {
@@ -71,23 +56,48 @@ const useBitCraft = () => {
 export default useBitCraft;
 
 // carousel
-export * from '@/components/carousel/carouselComposable';
-export * from '@/components/carousel/carouselConstants';
-export { default as CarouselSubscriber } from '@/components/carousel/CarouselSubscriber';
+export * from './components/carousel/carouselComposable';
+export * from './components/carousel/carouselConstants';
+export { default as CarouselSubscriber } from './components/carousel/CarouselSubscriber';
 
 // datatable
-export * from '@/components/datatable/datatableBuilders';
-export * from '@/components/datatable/datatableTypes';
+export * from './components/datatable/datatableBuilders';
+export * from './components/datatable/datatableTypes';
 
 // tabs
-export * from '@/components/tabs/TabGlobals';
+export * from './components/tabs/TabGlobals';
 
 // composables
-export * from '@/composables/cssClassTranslator';
-export * from '@/composables/cssGenerator';
-export { default as useCSSGenerator } from '@/composables/cssGenerator';
-export * from '@/composables/domNativeEventManager';
-export * from '@/composables/dragAndDrop';
-export * from '@/composables/onClickOutside';
-export { default as reactiveAsyncCallback } from '@/composables/reactiveAsyncCallback';
-export * from '@/composables/toastGenerator';
+export * from './composables/cssClassTranslator';
+export * from './composables/cssGenerator';
+export { default as useCSSGenerator } from './composables/cssGenerator';
+export * from './composables/domNativeEventManager';
+export * from './composables/dragAndDrop';
+export * from './composables/onClickOutside';
+export { default as reactiveAsyncCallback } from './composables/reactiveAsyncCallback';
+export * from './composables/toastGenerator';
+
+export type * from './composables/cssClassTranslator';
+export type * from './composables/cssGenerator';
+export type * from './composables/domNativeEventManager';
+export type * from './composables/dragAndDrop';
+export type * from './composables/onClickOutside';
+export type * from './composables/toastGenerator';
+
+// com
+
+export { default as CarouselCard } from './components/carousel/CarouselCard.vue';
+export { default as CarouselContainer } from './components/carousel/CarouselContainer.vue';
+export { default as CDatatable } from './components/datatable/CDatatable.vue';
+export { default as CDatatableCell } from './components/datatable/CDatatableCell.vue';
+export { default as CDatatableCellHeader } from './components/datatable/CDatatableCellHeader.vue';
+export { default as CDatatableHeader } from './components/datatable/CDatatableHeader.vue';
+export { default as CDatatableRow } from './components/datatable/CDatatableRow.vue';
+export { default as DragableSortable } from './components/dragable/DragableSortable.vue';
+export { default as DropdownComponent } from './components/dropdown/DropdownComponent.vue';
+export { default as BaseModal } from './components/modal/BaseModal.vue';
+export { default as StyledModal } from './components/modal/StyledModal.vue';
+export { default as TabMenu } from './components/tabs/TabMenu.vue';
+export { default as TabSlide } from './components/tabs/TabSlide.vue';
+export { default as ToastContainer } from './components/toast/ToastContainer.vue';
+export { default as ToastItem } from './components/toast/ToastItem.vue';
