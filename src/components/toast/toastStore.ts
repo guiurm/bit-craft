894baf30677fdf11f65bdb28a66254f5793b3566
guiurm <guiurm@gmail.com>
2024-10-24 21:26:08 +0200

    fix: remove pinia for toast store
    preventing project dependency errors

--- src/components/toast/toastStore.ts
+++ src/components/toast/toastStore.ts
@@ -1,11 +1,10 @@
 import type { ToastItemVNodeList } from '@/composables/toastGenerator';
-import { defineStore } from 'pinia';
 import { ref, type Ref } from 'vue';
 
-const useToastStore = defineStore('store', () => {
-    const stores = ref(new Map<string, ToastItemVNodeList>()) as Ref<Map<string, ToastItemVNodeList>>;
-    const first = ref(null) as Ref<null | string>;
+const stores = ref(new Map<string, ToastItemVNodeList>()) as Ref<Map<string, ToastItemVNodeList>>;
+const first = ref(null) as Ref<null | string>;
 
+const useToastStore = () => {
     const registerContainer = (id: string, items: ToastItemVNodeList) => {
         if (stores.value.size === 0) first.value = id;
         stores.value.set(id, items);
@@ -25,6 +24,6 @@ const useToastStore = defineStore('store', () => {
         removeStore,
         getList
     };
-});
+};
 
 export default useToastStore;
