f6aae6e9e3381320092e03e2ed5d1e6477d6df16
guiurm <guiurm@gmail.com>
2024-11-11 21:37:18 +0100

    feat(accordion): add AccordionComponent

---
+++ src/components/accordion/AccordionComponent.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { provide, type Ref, ref } from 'vue';
+
+export type TCollapseItem = Ref<{ uuid: string; isOpen: boolean }>;
+const props = withDefaults(defineProps<{ unique?: boolean }>(), { unique: false });
+const items = ref([]) as Ref<TCollapseItem[]>;
+
+provide('registerCollapse', (item: TCollapseItem) => {
+    items.value.push(item);
+});
+provide('manageToggleCollapse', (item: TCollapseItem) => {
+    let list = [] as TCollapseItem[];
+    if (props.unique) list = items.value.filter(i => i.value.isOpen && item.value.uuid !== i.value.uuid);
+    if (list.length > 0) list.forEach(i => (i.value.isOpen = false));
+});
+</script>
+
+<template>
+    <slot />
+</template>
--- src/components/collape/CollapseComponent.vue
+++ src/components/collape/CollapseComponent.vue
@@ -1,5 +1,8 @@
 <script lang="ts" setup>
-import { ref, watch } from 'vue';
+import { inject, onMounted, ref, watch } from 'vue';
+import { v6 } from 'uuid';
+import type { TCollapseItem } from '@/components/accordion/AccordionComponent.vue';
+import { noop } from "@/globals";
 
 type TProps = {
     // true to hide
@@ -13,35 +16,47 @@ defineSlots<{ header(): void; default(): void }>();
 const emits = defineEmits<{ 'update:model-value': [status: boolean]; open: []; close: [] }>();
 
 // values
-const isOpen = ref(props.modelValue);
+const collapseInfo = ref({ isOpen: props.modelValue, uuid: v6() });
+
+const registerCollapse = (inject('registerCollapse')??noop) as (item: TCollapseItem) => void;
+const manageToggleCollapse = (inject('manageToggleCollapse')??noop) as (item: TCollapseItem) => void;
 
 // watchers
 watch(
     () => props.modelValue,
     n => {
-        isOpen.value = n;
+        collapseInfo.value.isOpen = n;
         if (n) emits('open');
         else emits('close');
     }
 );
 
+watch(collapseInfo, newVal => (collapseInfo.value = newVal), { deep: true });
+
 // actions
-const toogle = () => {
-    isOpen.value = !isOpen.value;
-    emits('update:model-value', isOpen.value);
-    if (isOpen.value) emits('open');
+const toggle = () => {
+    collapseInfo.value.isOpen = !collapseInfo.value.isOpen;
+    emits('update:model-value', collapseInfo.value.isOpen);
+    if (collapseInfo.value.isOpen) emits('open');
     else emits('close');
+    manageToggleCollapse(collapseInfo);
 };
+
+// life-circle
+onMounted(() => {
+    registerCollapse(collapseInfo);
+});
 </script>
 <template>
     <div class="hs-collapse">
-        <div class="hs-collapse-header" @click="toogle">
+        {{ collapseInfo }}
+        <div class="hs-collapse-header bg-primary-500" @click="toggle">
             <slot name="header">
                 {{ header }}
             </slot>
         </div>
 
-        <div v-show="isOpen">
+        <div v-show="collapseInfo.isOpen">
             <slot />
         </div>
     </div>
